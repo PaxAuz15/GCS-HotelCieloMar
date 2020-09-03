6b40d6e115eb13f334734da7f10626113f529af4
Jordan Zamora <z.jordannoa03ci@gmail.com>
2020-09-03 17:01:38 -0500

    Modulo Habitaciones para empezar

--- ModuloHabitaciones/src/modulohabitaciones/ModuloHabitaciones.java
+++ ModuloHabitaciones/src/modulohabitaciones/ModuloHabitaciones.java
@@ -16,7 +16,7 @@ public class ModuloHabitaciones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Modulo Habitaciones subido");
+        System.out.println("Modulo Habitaciones ya subido");
     }
     
 }
