650faf1ff23ba508aade76221353b962a0025bc5
Peter Ramirez <peterramirez196@gmail.com>
2020-09-03 22:14:27 -0500

    Creacion de proyecto netbeans: Interfaz de registro

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author peter
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // TODO code application logic here
+        NewJFrame f = new NewJFrame();
+         f.setLocationRelativeTo(null);
+        f.setVisible (true);
+    }
+    
+}
